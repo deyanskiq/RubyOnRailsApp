f05a5bf64918c53352ffe7482addb2f214e6032c
Deyan Denchev <deyanskiq@abv.bg>
2018-01-12 19:12:33 +0200

    add stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'user#index'
+  root 'pages#home'
 end
